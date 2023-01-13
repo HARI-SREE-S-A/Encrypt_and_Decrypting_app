c87d2940a24d3ee42b5873952145b7beb9a561e7
HARISREE SURESH <getharisree@outlook.com>
2023-01-13 20:17:12 +0530

    Update creating_window_and_base.py

--- creating_window_and_base.py
+++ creating_window_and_base.py
@@ -1,1 +1,15 @@
+root = Tk()
+root.geometry('500x300')
+root.title("Encryptor")
+root.resizable(0,0)
 
+
+heading = Label(root,text = "Encryptor",fg = "white",bg = "blue",font = ("Calibri(Body)",22,'bold'))
+heading.pack()
+                
+footer = Label(root,text = "harvis labs",bg = "white",fg = "black",font = ("Calibri(Body)",10,'bold'))
+footer.pack(side = 'BOTTOM')
+
+
+
+root.mainloop()
