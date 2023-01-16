d4ecf1d6bc15c9d938f2c592f099f4f7dce13d3e
HARISREE SURESH <getharisree@outlook.com>
2023-01-16 17:35:45 +0530

    Update decoding.py

--- decoding.py
+++ decoding.py
@@ -1,1 +1,8 @@
+def Decode(keyy,message):
+    dec=[]
+    message = base64.urlsafe_b64decode(message).decode()
 
+    for i in range(len(message)):
+        key_c = keyy[i % len(keyy)]
+        dec.append(chr((256 + ord(message[i])- ord(key_c)) % 256))
+    return "".join(dec)
