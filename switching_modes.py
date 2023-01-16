07776e3c2f91022aa6cc3936b669d1193cf90e24
HARISREE SURESH <getharisree@outlook.com>
2023-01-16 17:36:45 +0530

    Update switching_modes.py

--- switching_modes.py
+++ switching_modes.py
@@ -1,1 +1,7 @@
-
+def moode():
+    m = vari.get()
+    if m == 'Encode':
+        result.set(encode(p_key.get(), Text.get()))
+    else:
+        result.set(Decode(p_key.get(), Text.get()))
+        
