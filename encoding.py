200f4f7a98ddc4adc270a0768b02db2ae21f8b56
HARISREE SURESH <getharisree@outlook.com>
2023-01-16 17:35:03 +0530

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -1,1 +1,8 @@
-
+def Encode(key,message):
+    enc=[]
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
+        print(enc)
+        
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
